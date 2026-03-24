6b8bd5ec57685ac308d4030a54e03f9966915f89
unknown <awballo22@gmail.com>
2026-03-24 21:24:54 +0000

    fix nodemailer pour l'envoi de mail en prod

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -8,19 +8,22 @@ const USER_EMAIL = process.env.EMAIL_USER;
 const APP_PASSWORD = process.env.EMAIL_APP_PASSWORD; 
 
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      auth: {
-        user: USER_EMAIL,
-        pass: APP_PASSWORD, 
-      },
-      port: 465,
-      secure: true,
-      family: 4,
-      connectionTimeout: 10000, 
-
-    });
-
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: USER_EMAIL,
+    pass: APP_PASSWORD,
+  },
+  // On force le timeout et la résolution DNS en IPv4
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+  dnsLookup: (hostname, options, callback) => {
+    require('dns').lookup(hostname, { family: 4 }, callback);
+  }
+});
     const info = await transporter.sendMail({
       from: `"Admin Gest Immo" <${USER_EMAIL}>`,
       to: destinataire,
