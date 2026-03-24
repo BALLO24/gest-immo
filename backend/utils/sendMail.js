d6b40cb8d01a550ff37c72fcf31272adcb699310
unknown <awballo22@gmail.com>
2026-03-24 21:42:15 +0000

    fix envoi mail en fixant une IP de smtp google

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -9,19 +9,17 @@ const APP_PASSWORD = process.env.EMAIL_APP_PASSWORD;
 
   try {
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "173.194.76.108", // C'est l'une des IP fixes de smtp.gmail.com
   port: 465,
   secure: true,
   auth: {
     user: USER_EMAIL,
     pass: APP_PASSWORD,
   },
-  // On force le timeout et la résolution DNS en IPv4
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-  dnsLookup: (hostname, options, callback) => {
-    require('dns').lookup(hostname, { family: 4 }, callback);
+  // On garde quand même TLS pour la sécurité
+  tls: {
+    rejectUnauthorized: false,
+    servername: 'smtp.gmail.com' // Crucial pour que le certificat SSL reste valide
   }
 });
     const info = await transporter.sendMail({
